634237ca5b828ba18beaa6e64a11df4938ed695b
Tobias Hüske <t.hueske@mittwald.de>
2024-02-18 12:06:51 +0100

    Check container

--- main.py
+++ main.py
@@ -23,13 +23,17 @@ def restart_target_container():
         print(f"Neustart abgebrochen, da der Cooldown von {restart_cooldown} Sekunden noch nicht abgelaufen ist.")
         return
     try:
-        containers = client.containers.list(all=True, filters={"name": container_name_to_restart})
-        for container in containers:
-            print(f"Versuche, Container {container.name} ({container.id}) neu zu starten...")
-            container.restart()
-            print(f"Container {container.name} erfolgreich neu gestartet.")
-            last_restart_time = time.time()
-            return
+        # Verwende den Service-Namen, um den Container zu identifizieren
+        containers = client.containers.list(all=True, filters={"label": f"com.docker.compose.service={container_name_to_restart}"})
+        if containers:
+            for container in containers:
+                print(f"Versuche, Container {container.name} ({container.id}) neu zu starten...")
+                container.restart()
+                print(f"Container {container.name} erfolgreich neu gestartet.")
+                last_restart_time = time.time()
+                return
+        else:
+            print(f"Keine Container gefunden, die dem Service {container_name_to_restart} entsprechen.")
     except APIError as e:
         print(f"Fehler beim Neustarten des Containers: {e}")
 
